bc5442df82ebab2538982864f2dd7e5f4416a656
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-12-03 19:21:16 -0500

    Completed Exercise 4: Changed country display format to table

--- world.php
+++ world.php
@@ -12,8 +12,28 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<?php if ($results): ?>
+  <table>
+    <thead>
+      <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results as $row): ?>
+        <tr>
+          <td><?= $row['name']; ?></td>
+          <td><?= $row['continent']; ?></td>
+          <td><?= $row['independence_year'] ?: ''; ?></td>
+          <td><?= $row['head_of_state'] ?: ''; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php else: ?>
+  <p>No results found.</p>
+<?php endif; ?>
